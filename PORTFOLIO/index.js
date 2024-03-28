ed9303ed1ce2311acb6b36819462fd3ad0fedccb
tatiana1818 <117773485+tatiana1818@users.noreply.github.com>
2024-03-28 12:40:00 -0500

    Add files via upload

---
+++ PORTFOLIO/index.js
@@ -0,0 +1,17 @@
+// gsap.to("#photo", {x:150, duration: 3})
+// gsap.from("p", {x:300, duration: 2})
+// gsap.to(".par", {x:50, duration: 4})
+// gsap.from("h4", {y:30, duration: 4, opacity: 0, delay: 2})
+// gsap.from("h2", {y: 30, duration: 3, opacity: 0, delay: 4} )
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//         anchor.addEventListener('click', function (e) {
+//             e.preventDefault();
+
+//             document.querySelector(this.getAttribute('href')).scrollIntoView({
+//                 behavior: 'smooth'
+//             });
+//         });
+//     });
+// });
